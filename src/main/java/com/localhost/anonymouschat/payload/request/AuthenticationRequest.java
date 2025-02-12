e787444f8cffc5d078282e0e1b289aa6ef7cd6c5
localhost <Adeayosegun5@gmail.com>
2025-02-12 02:17:04 +0100

    initial commit

---
+++ src/main/java/com/localhost/anonymouschat/payload/request/AuthenticationRequest.java
@@ -0,0 +1,20 @@
+package com.localhost.anonymouschat.payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    @NotBlank(message = "email is required")
+    @Email(message = "email format is not valid")
+    private String email;
+    @NotBlank(message = "password is required")
+    private String password;
+}
