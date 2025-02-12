d2390e837a695796e7a2c65cc4caa7ac19ca54ec
localhost <Adeayosegun5@gmail.com>
2025-02-12 02:20:38 +0100

    initial commit

---
+++ src/main/java/com/localhost/anonymouschat/payload/request/MessageRequest.java
@@ -0,0 +1,18 @@
+package com.localhost.anonymouschat.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageRequest {
+    @NotBlank
+    @Size(max = 2000)
+    private String content;
+}
