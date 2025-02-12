f2c3a0025c640c23adf9cb3c1acf8c43d9454081
localhost <Adeayosegun5@gmail.com>
2025-02-12 02:23:21 +0100

    initial commit

---
+++ src/main/java/com/localhost/anonymouschat/models/SecurityUser.java
@@ -0,0 +1,54 @@
+package com.localhost.anonymouschat.models;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class SecurityUser implements UserDetails {
+    // Add this method to get the original User
+    private final User user;
+
+    public SecurityUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList(); // or return actual authorities
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
