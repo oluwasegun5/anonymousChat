e9c8714c8047f4b92ec74108e85ae9920e03ed7d
localhost <Adeayosegun5@gmail.com>
2025-02-12 02:17:11 +0100

    initial commit

---
+++ src/main/java/com/localhost/anonymouschat/config/ApplicationSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.localhost.anonymouschat.config;
+
+import com.localhost.anonymouschat.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApplicationSecurityConfig {
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * we provide an implementation of the UserDetailsService functional interface, which has a loadByUsername method
+     * throws UsernameNotFoundException */
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return email -> userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
